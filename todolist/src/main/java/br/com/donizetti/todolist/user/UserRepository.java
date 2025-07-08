ed93b11611dd6c6c2e2f040ce84fa516da0e38d2
Donizetti de Lima Calheiros <donizettidev@gmail.com>
2025-07-08 09:32:00 -0300

    Criação do repository

---
+++ todolist/src/main/java/br/com/donizetti/todolist/user/UserRepository.java
@@ -0,0 +1,7 @@
+package br.com.donizetti.todolist.user;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<usermodel, UUID> {
+}
--- todolist/src/main/java/br/com/donizetti/todolist/user/usercontroller.java
+++ todolist/src/main/java/br/com/donizetti/todolist/user/usercontroller.java
@@ -3,15 +3,22 @@ package br.com.donizetti.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.var;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/users")
 public class usercontroller {
+    @Autowired
+    private UserRepository userRepository;
     
 @PostMapping("/")
-    public void create(@RequestBody usermodel usermodel){
-        System.out.println(usermodel.getUsername());
+    public usermodel create(@RequestBody usermodel usermodel){
+        var userCreated = this.userRepository.save(usermodel);
+            return userCreated;
 
     }
 }
