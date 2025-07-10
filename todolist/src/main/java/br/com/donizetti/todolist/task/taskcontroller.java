e1b0e11c4d7a7cd216ff55de5b095a5b37e21603
Donizetti de Lima Calheiros <donizettidev@gmail.com>
2025-07-10 13:47:34 -0300

    criação da area de task do codigo

---
+++ todolist/src/main/java/br/com/donizetti/todolist/task/taskcontroller.java
@@ -0,0 +1,20 @@
+package br.com.donizetti.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/task")
+public class taskcontroller {
+
+    @Autowired
+    private taskrepository iTaskrepository;
+    @PostMapping("/")
+    public void  create(@RequestBody taskmodel taskmodel){
+        var task = this.iTaskrepository.save(taskmodel);
+        
+    }
+}
