064f9bb59cc57df617e47ff16559fb7d590ce5a7
Donizetti de Lima Calheiros <donizettidev@gmail.com>
2025-07-27 19:45:36 -0300

    TODO LIST

--- todolist/src/main/java/br/com/donizetti/todolist/task/taskcontroller.java
+++ todolist/src/main/java/br/com/donizetti/todolist/task/taskcontroller.java
@@ -1,8 +1,13 @@
 package br.com.donizetti.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +22,31 @@ public class taskcontroller {
     @Autowired
     private taskrepository iTaskrepository;
     @PostMapping("/")
-    public void  create(@RequestBody taskmodel taskmodel, HttpServletRequest request){
+    public ResponseEntity<?> create(@RequestBody taskmodel taskmodel, HttpServletRequest request) {
         var iduser = (UUID) request.getAttribute("iduser");
-        taskmodel.setUserid(null);
+        taskmodel.setUserid(iduser); // Corrige para setar o id do usuário
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskmodel.getStartaT())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(taskmodel);
+        }
+        if (currentDate.isAfter(taskmodel.getEndat())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(taskmodel);
+        }
+
         var task = this.iTaskrepository.save(taskmodel);
-        
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+
     }
+    @GetMapping("/")
+    public List<taskmodel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("iduser");
+        var tasks = this.iTaskrepository.findByUserid((UUID) idUser);
+        return tasks;
+    }
+    public void update(@RequestBody taskmodel taskmodel, HttpServletRequest request) {
+    
+    }
+
 }
+
--- todolist/src/main/java/br/com/donizetti/todolist/task/taskrepository.java
+++ todolist/src/main/java/br/com/donizetti/todolist/task/taskrepository.java
@@ -1,10 +1,11 @@
 package br.com.donizetti.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface taskrepository extends JpaRepository<taskmodel, UUID>{
-    
+    List<taskmodel> findByUserid(UUID userid);
     
 }
